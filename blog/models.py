cb64d33a1adfb50f410c73af076e96dd81c10320
Верхотуров Александр Александрович <Verkhoturov.A@partner.ru>
2022-04-07 16:31:31 +1000

    добавил возможност создания постов

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    post_title = models.CharField(max_length=150)
+    post_date = models.DateTimeField(null=True)
+    post_text = models.TextField(blank=True)
+    post_image = models.ImageField(upload_to = 'blog_mages/')
