3f83cef1cd74f07380f8ddedadafa20407c27e6a
Verkhoturov.A <verkhoturov.a@dns-shop.ru>
2022-04-15 11:58:58 +1000

    .

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from events.models import Event
 
 # Create your views here.
+def showblog(request):
+    posts = Post.objects
+    events = Event.objects
+    return render(request, 'blog/blog.html',{'posts': posts,'events':events})
--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+def home(request):
+    #Получаем все объекты класса Event
+    events = Event.objects
+    return render(request, 'events/home.html', {'events': events})
